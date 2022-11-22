346f6562e6cb34b25411ac9c0594a116366dd3df
MarkiGa <73349060+MarkiGa@users.noreply.github.com>
2022-11-22 17:22:31 +0100

    kim anpassungen

---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Application;
+
+public class OpenURL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.OpenURL("https://www.unipark.de/uc/b_hu_inst_psych_psying/22ca/");
+    }
+
+}
